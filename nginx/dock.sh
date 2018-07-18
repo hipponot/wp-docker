7c0d2b89fb8613577e44d8d76ae48d486f045941
Brent Milne <brent.milne@nimbee.com>
2018-07-18 14:16:14 -0600

    added repo directory to path

--- nginx/dock.sh
+++ nginx/dock.sh
@@ -6,7 +6,7 @@ docker run -p 81:80 -p 443:443 \
     --name nginx \
     --network=dock_net \
     -v /root/var/www:/var/www \
-    -v /root/nginx/wp-docker-nginx-conf.d:/etc/nginx/conf.d \
+    -v /root/wp-docker/nginx/wp-docker-nginx-conf.d:/etc/nginx/conf.d \
     -v /root/ssl:/etc/ssl:ro \
     -d nginx
 

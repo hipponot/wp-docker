b4a1528f3ed557a68fbbe169801ff32b22f4b866
Brent Milne <brent.milne@nimbee.com>
2018-07-18 14:38:10 -0600

    -

--- doc_net.sh
+++ doc_net.sh
@@ -2,6 +2,6 @@
 
 docker network create --driver bridge dock_net
 
-docker network connect dock_net nginx
+docker network connect dock_net nginx-frontent
 docker network connect dock_net woot-math-wordpress
 docker network connect dock_net web-content-nginx
--- nginx/dock.sh
+++ nginx/dock.sh
@@ -2,11 +2,12 @@
 
 # docker network create --driver bridge dock_net
 
-docker run -p 81:80 -p 443:443 \
-    --name nginx \
+docker run --detach \
+    --name nginx-frontend \
     --network=dock_net \
     -v /root/var/www:/var/www \
     -v /root/wp-docker/nginx/etc:/etc/nginx/ \
     -v /root/ssl:/etc/ssl:ro \
-    -d nginx
+    -p 81:80 -p 443:443 
+    nginx
 

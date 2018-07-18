1ad22b2efb6a11c540f1f00f5004d0ff1e945dcf
Brent Milne <brent.milne@nimbee.com>
2018-07-18 16:03:22 -0600

    -

--- nginx/dock.sh
+++ nginx/dock.sh
@@ -5,8 +5,11 @@
 docker run --detach \
     --name nginx-frontend \
     --network=dock_net \
+    -v /root/var-nginx-www:/var/www \
     -v /root/wp-docker/nginx/etc:/etc/nginx/ \
     -v /root/ssl:/etc/ssl:ro \
     -p 81:80 -p 443:443 \
     nginx
 
+
+    # -v /root/var/www:/var/www \
